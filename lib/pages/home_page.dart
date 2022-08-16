f5e5191ac5c9f6fd584fd809e422e02e6a871c4b
Raphael Silva <raphaeltiago02@gmail.com>
2022-08-16 09:40:41 -0300

    '-'

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: sized_box_for_whitespace, must_be_immutable, avoid_unnecessary_containers
 
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:movies/pages/movies_page.dart';
 import 'package:movies/pages/series_page.dart';
@@ -12,6 +15,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool hasConnection = false;
+
   int tabIndex = 0;
 
   List<Widget> pages = [
@@ -19,8 +24,68 @@ class _HomePageState extends State<HomePage> {
     const SeriesPage(),
   ];
 
+  Future<void> checkConnectivity() async {
+    var connection = await Connectivity().checkConnectivity();
+
+    _updateConnectionStatus(connection);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        hasConnection = true;
+      } else {
+        hasConnection = false;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    checkConnectivity();
+    // StreamSubscription<ConnectivityResult> stream =
+    Connectivity().onConnectivityChanged.listen((connection) {
+      _updateConnectionStatus(connection);
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // no connection screen
+    if (!hasConnection) {
+      return Scaffold(
+        backgroundColor: const Color(0xff111111),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.wifi_off_rounded,
+                  size: 60,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Não conectado à internet!',
+                  style: TextStyle(fontSize: 28),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Tente se conectar e tente novamente.',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       body: pages[tabIndex],
       bottomNavigationBar: SizedBox(
